3bedfc38c310487fd373f5e98d1aea1c0ccccb2f
Escalion <escalion86@gmail.com>
2026-09-08 14:55:40 +0700

    feat: умное поле локации в форме события (DaData + пул + ручной ввод)

--- components/AddressPoolPicker.js
+++ components/AddressPoolPicker.js
@@ -3,7 +3,6 @@
 import cn from 'classnames'
 import { useMemo, useState } from 'react'
 import { useAtom } from 'jotai'
-import { faPencilAlt } from '@fortawesome/free-solid-svg-icons/faPencilAlt'
 import { postData } from '@helpers/CRUD'
 import {
   formatAddressPoolShort,
@@ -11,10 +10,9 @@ import {
 } from '@helpers/addressPool'
 import siteSettingsAtom from '@state/atoms/siteSettingsAtom'
 import InputWrapper from './InputWrapper'
-import ComboBox from './ComboBox'
 import Button from './Button'
-import IconActionButton from './IconActionButton'
 import AddressPicker from './AddressPicker'
+import AddressSuggestField from './AddressSuggestField'
 
 const AddressPoolPicker = ({
   address,
@@ -33,11 +31,8 @@ const AddressPoolPicker = ({
   fieldsVariant = 'artist',
   outerClassName,
   contentClassName,
-  comboBoxLabel = 'Выбрать адрес',
-  emptyComboBoxPlaceholder = 'Не указан',
   saveButtonLabel = 'Сохранить в пул',
   savedLabel = '✓ В пуле',
-  manualToggleTitles,
 }) => {
   const isParty = tone === 'party'
   const [siteSettings, setSiteSettings] = useAtom(siteSettingsAtom)
@@ -49,15 +44,6 @@ const AddressPoolPicker = ({
         ? controlledPoolAddresses
         : (siteSettings?.addresses ?? []),
     [controlledPoolAddresses, siteSettings?.addresses]
-  )
-
-  const poolOptions = useMemo(
-    () =>
-      poolAddresses.map((addr) => ({
-        name: formatAddressPoolShort(addr),
-        value: JSON.stringify(addr),
-      })),
-    [poolAddresses]
   )
 
   const formattedAddress = useMemo(
@@ -73,32 +59,10 @@ const AddressPoolPicker = ({
     )
   }, [address, formattedAddress, poolAddresses])
 
-  const currentValue = useMemo(() => {
-    if (!formattedAddress) return null
-    return isAddressInPool ? JSON.stringify(address) : null
-  }, [address, formattedAddress, isAddressInPool])
-
   const canSaveToPool = useMemo(() => {
     if (!address) return false
     return address.town || address.street || address.house
   }, [address])
-
-  const resolvedComboBoxPlaceholder = useMemo(() => {
-    return formattedAddress || emptyComboBoxPlaceholder
-  }, [emptyComboBoxPlaceholder, formattedAddress])
-
-  const handleSelectFromPool = (value) => {
-    if (!value) {
-      onChange?.(null)
-      return
-    }
-
-    try {
-      onChange?.(JSON.parse(value))
-    } catch {
-      onChange?.(null)
-    }
-  }
 
   const handleSaveToPool = async () => {
     if (!address) return
@@ -128,35 +92,15 @@ const AddressPoolPicker = ({
       >
         <div className={cn('mt-0.5 mb-1 min-w-0 flex-1', contentClassName)}>
           <div className="flex flex-col">
-            <div className="grid grid-cols-[minmax(0,1fr)_auto] items-end gap-x-2">
-              <div className="relative mt-2.5 w-full min-w-0 flex-1">
-                <ComboBox
-                  label={comboBoxLabel}
-                  items={poolOptions}
-                  value={currentValue}
-                  onChange={handleSelectFromPool}
-                  placeholder={resolvedComboBoxPlaceholder}
-                  activePlaceholder
-                  tone={tone}
-                  noMargin
-                  fullWidth
-                  error={errors?.address}
-                  className="w-full min-w-0 flex-1"
-                  selectClassName="min-w-0 overflow-hidden text-ellipsis truncate"
-                />
-              </div>
-              <IconActionButton
-                icon={faPencilAlt}
-                onClick={() => setShowManualInput((value) => !value)}
-                title={
-                  showManualInput
-                    ? manualToggleTitles?.collapse || 'Свернуть ввод адреса'
-                    : manualToggleTitles?.expand || 'Ввести адрес'
-                }
-                size="md"
-                variant="neutral"
-              />
-            </div>
+            <AddressSuggestField
+              address={address}
+              onChange={onChange}
+              poolAddresses={poolAddresses}
+              defaultTown={siteSettings?.defaultTown ?? ''}
+              manualOpen={showManualInput}
+              onManualInput={() => setShowManualInput((value) => !value)}
+              error={errors?.address}
+            />
 
             {showManualInput && (
               <div className="flex flex-col">
